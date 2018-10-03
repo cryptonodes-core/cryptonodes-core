1332b247a7407d7a7dcd5f7e2fa6796cf99bbede
cryptonodes-core <system@cryptonodes.ch>
2018-10-03 12:31:28 +0200

    fix util, simulate script

--- src/test/bitcoin-util-test.py
+++ src/test/bitcoin-util-test.py
@@ -4,9 +4,10 @@
 # Distributed under the MIT/X11 software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-# import os
-# import bctest
-# import buildenv
+import os
+import bctest
+import buildenv
+
 #
 # if __name__ == '__main__':
 # 	bctest.bctester(os.environ["srcdir"] + "/test/data",
